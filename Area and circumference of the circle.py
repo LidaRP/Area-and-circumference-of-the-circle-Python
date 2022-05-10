e3c4c9c6deb754b40747ff439ca54cca210fa8e5
LidaRP <rahmanipiyani.lida@gmail.com>
2022-05-10 13:53:56 +0430

    Area and circumference of the circle

---
+++ Area and circumference of the circle.py
@@ -0,0 +1,7 @@
+a = float(input('Please enter the radius of the circle : '))
+
+perim = 2 * 3.14 * a
+area = 3.14 * (a ** 2)
+
+print('The circumference of the circle is equal to :', perim)
+print('The area of the circle is equal to :', area)
